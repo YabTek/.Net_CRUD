da67f03796ca1e5aca01c7418b5a92caf5a48d9b
YabTek <yabtekuam@gmail.com>
2023-04-12 13:45:26 +0300

    add crud for team model

--- Controllers/TeamsController.cs
+++ Controllers/TeamsController.cs
@@ -7,7 +7,7 @@ namespace Crud.Controllers;
 [ApiController]
 
 public class TeamsController : ControllerBase{
-    private List<Team> teams = new List<Team>(){
+    private static List<Team> teams = new List<Team>(){
         new Team() {
              Country = "Ethiopia",
              id = 1,
@@ -35,4 +35,51 @@ public class TeamsController : ControllerBase{
         return Ok(teams);
     }
 
+    [HttpGet("{id:int}")]
+
+    public IActionResult Get(int id){
+
+       var team = teams.FirstOrDefault(x => x.id == id);
+
+       if (team  == null){
+        return BadRequest("team is not found");
+       }
+       return Ok(team);
+    }
+
+    [HttpPost]
+
+    public IActionResult Post(Team team){
+        teams.Add(team);
+
+        return CreatedAtAction("Get", routeValues:team.id, value:team);
+    }
+
+    [HttpPatch]
+
+    public IActionResult Patch(int id, string country){
+       var team = teams.FirstOrDefault(x => x.id == id);
+
+       if (team == null){
+        return BadRequest("team is not found");
+       }
+
+       team.Country = country;
+
+       return NoContent();
+    }
+
+    [HttpDelete]
+
+    public IActionResult Delete(int id){
+        var team = teams.FirstOrDefault(x => x.id == id);
+
+        if (team== null){
+            return BadRequest("team is not found");
+
+        }
+        teams.Remove(team);
+        
+        return NoContent();
+    }
 }
